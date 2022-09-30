c9de4a08aadb669507e9a04d82e3c66fcb72cdf2
Asia <asia_csharpcourse@gmail.com>
2022-09-30 20:55:41 +0200

    fridge category and models created

--- MagicDish.Core/Models/FridgeProduct.cs
+++ MagicDish.Core/Models/FridgeProduct.cs
@@ -3,14 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MagicDish.Core.Models
 {
-    public class FridgeProduct : ProductQuantity
+    public class FridgeProduct
     {
         [Key]
         public int Id { get; set; }
+
+        public int Amount { get; set; }
 
         public int FridgeId { get; set; }
 
         [ForeignKey("FridgeId")]
         public Fridge Fridge { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
     }
 }
--- MagicDish.Core/Models/Ingridient.cs
+++ MagicDish.Core/Models/Ingridient.cs
@@ -3,14 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MagicDish.Core.Models
 {
-    public class Ingridient : Product
+    public class Ingridient
     {
         [Key]
         public int Id { get; set; }
 
+        public int Quantity { get; set; }
+
         public int RecipeId { get; set; }
 
         [ForeignKey("RecipeId")]
-        Recipe Recipe { get; set; } 
+        Recipe Recipe { get; set; }
+        
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
     }
 }
